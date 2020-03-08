2037e586cfc239f3a8a6140cb0fa2f5de45340fe
Tyler Auer <Tyler.Auer@gmail.com>
2020-03-07 20:52:15 -0800

    migrate about page

---
+++ src/pages/about.js
@@ -0,0 +1,27 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const AboutPage = () => (
+  <Layout>
+    <div className="container">
+      <h2>About</h2>
+      <p>
+        As a math teacher interested in code and design I like to puzzle through
+        ways to help others make sense of complex ideas. I'm working to
+        strengthen my coding skills by building interactives for others to
+        explore.
+      </p>
+      <p>
+        Much of what lives here is fairly old. It works, but the code isn’t
+        pretty. There are too many things done inefficiently, too much repeated
+        code. I didn’t know any better.
+      </p>
+      <p>
+        I’ve updated a few things here and there, but the goal is really to keep
+        building new things with better practices and skills.
+      </p>
+    </div>
+  </Layout>
+)
+
+export default AboutPage
