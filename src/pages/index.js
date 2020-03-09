32ca7cac24e0822626ac0784fa3d56eb6cf31723
Tyler Auer <Tyler.Auer@gmail.com>
2020-03-08 19:50:17 -0700

    remove .html from links

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,13 +9,13 @@ const IndexPage = () => (
         <Card
           title="100s Grid"
           desc="Explore place value, skip counting and factors with this customizable 100s chart."
-          btnUrl="/apps/grid.html"
+          btnUrl="/apps/100s-grid"
           btnText="Where we're going, we don't need roads."
         />
         <Card
           title="Multiplication Table"
           desc="Discover new patterns in this customizable multiplication chart."
-          btnUrl="/apps/mult.html"
+          btnUrl="/apps/mult-table"
           btnText="Spared no expense."
         />
       </div>
@@ -23,13 +23,13 @@ const IndexPage = () => (
         <Card
           title="Monte Carlo"
           desc="Run random events over and over and over and over..."
-          btnUrl="/apps/monte-carlo.html"
+          btnUrl="/apps/monte-carlo"
           btnText="I, too, like to live dangerously."
         />
         <Card
           title="Half Life"
           desc="Explore how probability drives the decay of radioactive materials. Learn how rates of decay are measured using half-life."
-          btnUrl="/apps/half-life.html"
+          btnUrl="/apps/half-life"
           btnText="The files are in the computer?"
         />
       </div>
