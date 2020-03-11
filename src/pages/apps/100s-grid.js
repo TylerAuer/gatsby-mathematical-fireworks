43f8d904485b8598c0b85e95b6761013409d5bbf
Tyler Auer <Tyler.Auer@gmail.com>
2020-03-10 21:32:14 -0700

    user inputs adjust grid live:
    - Can change all 4 inputs
    - Catch errors when input is ""; defaults to 1
    - Reset button resets to standard hundreds grid

--- src/pages/apps/100s-grid.js
+++ src/pages/apps/100s-grid.js
@@ -39,13 +39,15 @@ function InputField(props) {
   }
   return (
     <div className="col-xs-12 col-sm-6 col-md text-center">
-      <label for="start">
-        <h5 style={{ margin: "25px auto 0px auto" }}>{title}</h5>
+      <label htmlFor="start">
+        <h5 style={{ margin: "25px auto 0px auto" }}>{props.title}</h5>
       </label>
       <input
         style={style}
         id={props.name}
         name={props.name}
+        value={props.value}
+        onChange={props.onChange}
         type="number"
         className="form-control text-center"
         placeholder={props.placeholder}
@@ -54,19 +56,6 @@ function InputField(props) {
         step="1"
       />
     </div>
-  )
-}
-
-function UserInputs(props) {
-  return (
-    <form style={{ margin: "0px auto 30px auto" }}>
-      <div className="form-row">
-        <InputField name="start" placeholder="1" min="0" max="10000" />
-        <InputField name="end" placeholder="100" min="1" max="10000" />
-        <InputField name="skip-size" placeholder="1" min="1" max="1250" />
-        <InputField name="columns" placeholder="1" min="1" max="150" />
-      </div>
-    </form>
   )
 }
 
@@ -79,15 +68,51 @@ function Cell(props) {
     padding: "7px",
   }
   return (
-    <td className="cell-in-100s-grid" style={cellStyle}>
+    <td id={"cell" + props.id} className="cell-in-100s-grid" style={cellStyle}>
       {props.value}
     </td>
   )
 }
 
-class Grid extends React.Component {
+function Grid(props) {
+  const rowsNeeded = Math.ceil(
+    Math.ceil((1 + (props.endNum - props.startNum)) / props.skipSize) /
+      props.columns
+  )
+  let grid = []
+  let counter = props.startNum
+  // loop for row
+  for (let i = 0; i < rowsNeeded; i++) {
+    let row = []
+    // loop for nums in each row
+    for (let j = 0; j < props.columns; j++) {
+      if (counter <= props.endNum) {
+        row.push(<Cell id={counter} key={counter} value={counter} />)
+      } else {
+        row.push(<Cell id={counter} key={counter} value=" " />)
+      }
+      counter += props.skipSize
+    }
+    grid.push(<tr key={i}>{row}</tr>)
+  }
+
+  const tableStyles = {
+    width: "100%",
+    borderStyle: "hidden",
+  }
+
+  return (
+    <table style={tableStyles}>
+      <tbody>{grid}</tbody>
+    </table>
+  )
+}
+
+class GridApp extends React.Component {
   constructor(props) {
     super(props)
+    this.handleChange = this.handleChange.bind(this)
+    this.resetOnClick = this.resetOnClick.bind(this)
     this.state = {
       startNum: 1,
       endNum: 100,
@@ -96,55 +121,98 @@ class Grid extends React.Component {
     }
   }
 
+  resetOnClick(e) {
+    console.log("It fired")
+
+    this.setState({
+      startNum: 1,
+      endNum: 100,
+      skipSize: 1,
+      columns: 10,
+    })
+  }
+
+  handleChange(e) {
+    if (e.target.value === "") {
+      // Avoids crash when user deletes current value in input
+      this.setState({ [e.target.name]: 1 })
+    } else {
+      this.setState({ [e.target.name]: parseInt(e.target.value) })
+    }
+  }
+
   render() {
-    const rowsNeeded = Math.ceil(
-      Math.ceil(
-        (1 + (this.state.endNum - this.state.startNum)) / this.state.skipSize
-      ) / this.state.columns
+    return (
+      <Layout>
+        <div
+          style={{ margin: "10px auto" }}
+          className="container text-center"
+          id="button-bank"
+        >
+          <ControlBtn
+            className="btn btn-danger"
+            text="Reset"
+            onClick={this.resetOnClick}
+          />
+          {/* <ButtonBank
+            startNum={this.state.startNum}
+            endNum={this.state.endNum}
+            skipSize={this.state.skipSize}
+            columns={this.state.columns}
+          /> */}
+        </div>
+        <div style={{ margin: "10px auto" }} id="settings-bank">
+          <form style={{ margin: "0px auto 30px auto" }}>
+            <div className="form-row">
+              <InputField
+                title="Start"
+                name="startNum"
+                value={this.state.startNum}
+                onChange={this.handleChange}
+                min="-100"
+                max="10000"
+              />
+              <InputField
+                title="End"
+                name="endNum"
+                value={this.state.endNum}
+                onChange={this.handleChange}
+                min="1"
+                max="10000"
+              />
+              <InputField
+                title="Skip Size"
+                name="skipSize"
+                value={this.state.skipSize}
+                onChange={this.handleChange}
+                min="1"
+                max="1250"
+              />
+              <InputField
+                title="Columns"
+                name="columns"
+                value={this.state.columns}
+                onChange={this.handleChange}
+                min="1"
+                max="150"
+              />
+            </div>
+          </form>
+        </div>
+        <div
+          id="grid-container"
+          style={{ overflowX: "auto", margin: "10px auto" }}
+        >
+          <Grid
+            startNum={this.state.startNum}
+            endNum={this.state.endNum}
+            skipSize={this.state.skipSize}
+            columns={this.state.columns}
+          />
+        </div>
+      </Layout>
     )
-
-    let grid = []
-    let counter = this.state.startNum
-    // loop for row
-    for (let i = 0; i < rowsNeeded; i++) {
-      let row = []
-      // loop for nums in each row
-      for (let j = 0; j < this.state.columns; j++) {
-        if (counter <= this.state.endNum) {
-          row.push(<Cell key={counter} value={counter} />)
-        } else {
-          row.push(<Cell key={counter} value=" " />)
-        }
-        counter += this.state.skipSize
-      }
-      grid.push(<tr>{row}</tr>)
-    }
-
-    const tableStyles = {
-      width: "100%",
-      borderStyle: "hidden",
-    }
-
-    return <table style={tableStyles}>{grid}</table>
   }
 }
-
-const GridApp = () => (
-  <Layout>
-    <div
-      style={{ margin: "10px auto" }}
-      className="container text-center"
-      id="button-bank"
-    >
-      <ButtonBank />
-    </div>
-    <div style={{ margin: "10px auto" }} id="settings-bank">
-      <UserInputs />
-    </div>
-    <div id="grid-container" style={{ overflowX: "auto", margin: "10px auto" }}>
-      <Grid />
-    </div>
-  </Layout>
-)
 
 export default GridApp
