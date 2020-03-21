f735309c0a7fb888de9d53411b7b5ba8e8ffce11
Tyler Auer <Tyler.Auer@gmail.com>
2020-03-21 13:33:14 -0700

    add click to shade

--- src/pages/apps/mult-table.js
+++ src/pages/apps/mult-table.js
@@ -21,13 +21,26 @@ const MultTable = props => {
   props.rowFactors.forEach(rowNum => {
     let row = [<th className="mult-table-header">{rowNum}</th>]
     props.colFactors.forEach(colNum => {
-      row.push(<Cell value={rowNum * colNum} shadedCells={props.shadedCells} />)
+      row.push(
+        <Cell
+          id={"row" + rowNum + "col" + colNum}
+          value={rowNum * colNum}
+          shadedCells={props.shadedCells}
+          onClick={props.onClick}
+        />
+      )
     })
     arrOfRows.push(<tr>{row}</tr>)
   })
 
   return (
-    <table>
+    <table
+      css={css`
+        width: 98%;
+        margin: 5px auto;
+        text-align: center;
+      `}
+    >
       <thead>{headerRow}</thead>
       <tbody>{arrOfRows}</tbody>
     </table>
@@ -37,21 +50,46 @@ const MultTable = props => {
 class MultTableApp extends React.Component {
   constructor(props) {
     super(props)
+    this.cellOnClick = this.cellOnClick.bind(this)
     this.state = {
       colFactors: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 144],
-      rowFactors: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      rowFactors: [2, 4, 6, 8, 10, 1, 3, 5, 7, 9],
       shadedCells: [],
+    }
+  }
+
+  cellOnClick(e) {
+    const cellId = e.target.id
+    // remove cell from shadedCells (removes highlights)
+    if (this.state.shadedCells.includes(cellId)) {
+      const shadedCells = this.state.shadedCells
+      const newShadedCells = shadedCells.filter(id => id !== cellId)
+      this.setState({ shadedCells: newShadedCells })
+
+      // add cell Id to shadedCells (removes highlights)
+    } else {
+      const newShadedCells = this.state.shadedCells
+      newShadedCells.push(cellId)
+      this.setState({ shadedCells: newShadedCells })
     }
   }
 
   render() {
     return (
       <Layout>
-        <MultTable
-          colFactors={this.state.colFactors}
-          rowFactors={this.state.rowFactors}
-          shadedCells={this.state.shadedCells}
-        />
+        <div
+          css={css`
+            overflow-x: auto;
+            margin: 10px auto;
+          `}
+        >
+          <MultTable
+            colFactors={this.state.colFactors}
+            rowFactors={this.state.rowFactors}
+            shadedCells={this.state.shadedCells}
+            onClick={this.cellOnClick}
+          />
+        </div>
       </Layout>
     )
   }
