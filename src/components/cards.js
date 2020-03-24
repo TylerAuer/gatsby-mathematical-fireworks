2cbde72c58316aec252edcc4e228c1d9657400ca
Tyler Auer <Tyler.Auer@gmail.com>
2020-03-24 13:27:18 -0700

    add mousover transition to nav buttons

--- src/components/cards.js
+++ src/components/cards.js
@@ -10,7 +10,14 @@ import { css } from "@emotion/core"
 
 const Card = props => (
   <div className="col-lg-6 mb-3">
-    <div className="text-center card h-100 ">
+    <div
+      className="text-center card h-100 "
+      css={css`
+        :hover {
+          border: solid 1px rgba(255, 0, 141, 1);
+        }
+      `}
+    >
       <div className="card-body">
         <h2 className="card-title text-center">{props.title}</h2>
         <p className="card-text">{props.desc}</p>
@@ -18,7 +25,6 @@ const Card = props => (
       <div
         css={css`
           background-color: white;
-          padding: 25px;
         `}
       >
         <a
@@ -27,11 +33,16 @@ const Card = props => (
           css={css`
             background-color: rgba(255, 0, 141, 0.85);
             color: white;
-            font-family: "Bungee", cursive;
+            /* font-family: "Bungee", cursive; */
             font-size: 15px;
+            font-weight: bolder;
+            margin: 0px auto 10px auto;
+            width: 90%;
+            transition: width ease-in-out 500ms;
             &:hover {
               color: white;
-              background-color: rgba(255, 0, 141, 1);
+              background-color: rgb(255, 116, 81);
+              width: 95%;
             }
           `}
         >
