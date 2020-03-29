89af8a0b3133d759bfe3dbddc6de6ed03dc5e538
Tyler Auer <Tyler.Auer@gmail.com>
2020-03-29 10:26:23 -0700

    wrap header in container-fluid to hor scroll bug

--- src/components/title-and-nav/header.js
+++ src/components/title-and-nav/header.js
@@ -4,17 +4,19 @@ import Title from "./title"
 
 const Header = () => (
   <header>
-    <Title text="Mathematical Playgrounds" />
+    <div className="container-fluid">
+      <Title text="Mathematical Playgrounds" />
 
-    <div id="nav-links" className="mb-3">
-      <nav className="nav d-flex justify-content-center">
-        <NavLink url="/" text="Home" />
-        <NavLink url="/about" text="About" />
-        <NavLink
-          url="https://github.com/TylerAuer/gatsby-mathematical-fireworks"
-          text="GitHub"
-        />
-      </nav>
+      <div id="nav-links" className="mb-3">
+        <nav className="nav d-flex justify-content-center">
+          <NavLink url="/" text="Home" />
+          <NavLink url="/about" text="About" />
+          <NavLink
+            url="https://github.com/TylerAuer/gatsby-mathematical-fireworks"
+            text="GitHub"
+          />
+        </nav>
+      </div>
     </div>
   </header>
 )
