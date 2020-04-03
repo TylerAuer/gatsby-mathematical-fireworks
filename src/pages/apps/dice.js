a40d15ec378a32b361c041eeb2d49b1d8d39bb35
Tyler Auer <Tyler.Auer@gmail.com>
2020-04-03 12:59:56 -0700

    run, stop, reset now control the simulation

--- src/pages/apps/dice.js
+++ src/pages/apps/dice.js
@@ -3,9 +3,34 @@ import Layout from "../../components/layout"
 import AppIntro from "../../components/intro"
 import InputField from "../../components/inputField"
 import Die from "../../components/die"
+import DataDisplay from "../../components/dataDisplay"
+import ControlBtn from "../../components/control-btn"
+import { css } from "@emotion/core"
 // import { Bar } from "react-chartjs-2"
 
-// const data = [12, 19, 3, 5, 2, 3]
+const ctrlBtnStyle = css`
+  margin: 3px 2px;
+  font-family: "Bungee", cursive;
+  background-color: rgba(20, 186, 204, 1);
+  color: white;
+  &:hover,
+  &:focus {
+    background-color: rgb(92, 221, 41);
+    color: white;
+  }
+`
+
+const resetBtnStyle = css`
+  margin: 3px 2px;
+  font-family: "Bungee", cursive;
+  background-color: rgb(255, 116, 81);
+  color: white;
+  &:hover,
+  &:focus {
+    color: white;
+    background-color: rgba(255, 0, 141, 1);
+  }
+`
 
 const intro = (
   <>
@@ -64,10 +89,15 @@ class DiceApp extends React.Component {
     super(props)
     this.chartReference = React.createRef()
     this.diceCountOnChange = this.diceCountOnChange.bind(this)
+    this.startOnClick = this.startOnClick.bind(this)
+    this.stopOnClick = this.stopOnClick.bind(this)
+    this.resetOnClick = this.resetOnClick.bind(this)
     this.state = {
       iterations: 0,
       diceCount: 2,
       lastRoll: ["?", "?"],
+      resultCounts: new Array(11).fill(0), // counts of each sum
+      avgSumHist: [], // average over time
     }
   }
 
@@ -76,6 +106,7 @@ class DiceApp extends React.Component {
   }
 
   diceCountOnChange(e) {
+    this.stop()
     let newDiceCount = parseInt(e.target.value)
     const maxDiceCount = 20
     if (newDiceCount < 1) {
@@ -86,22 +117,101 @@ class DiceApp extends React.Component {
     this.setState({
       diceCount: newDiceCount,
       lastRoll: new Array(newDiceCount).fill("?"),
+      resultCounts: new Array(5 * newDiceCount + 1).fill(0),
+      avgSumHist: [],
+    })
+  }
+
+  getRandomInt(min, max) {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min)) + min //The maximum is exclusive and the minimum is inclusive
+  }
+
+  runSim() {
+    const newLastRoll = this.state.lastRoll.map(() => {
+      return this.getRandomInt(1, 7)
+    })
+
+    const sumOfLastRoll = newLastRoll.reduce((total, num) => {
+      return total + num
+    })
+
+    let newResultCounts = this.state.resultCounts
+    newResultCounts[sumOfLastRoll - this.state.diceCount] += 1 // must go back diceCount because 0-index and smallest value = diceCount
+
+    let newAvg
+    if (this.state.avgSumHist.length) {
+      newAvg =
+        (this.state.avgSumHist[this.state.avgSumHist.length - 1] *
+          this.state.iterations +
+          sumOfLastRoll) /
+        (this.state.iterations + 1)
+    } else newAvg = sumOfLastRoll
+    let newAvgSumHist = this.state.avgSumHist
+    newAvgSumHist.push(newAvg)
+
+    console.log("------------------")
+    console.log("Roll:", newLastRoll)
+    console.log("Sum:", sumOfLastRoll)
+    console.log("Counts:", newResultCounts)
+    console.log("AVG:", newAvg)
+    this.setState(state => {
+      return {
+        iterations: state.iterations + 1,
+        lastRoll: newLastRoll,
+        resultCounts: newResultCounts,
+        avgSumHist: newAvgSumHist,
+      }
+    })
+  }
+
+  startOnClick(e) {
+    this.simHandle = setInterval(() => {
+      this.runSim()
+    }, 1)
+  }
+
+  stop() {
+    clearInterval(this.simHandle)
+  }
+
+  stopOnClick(e) {
+    this.stop()
+  }
+
+  resetOnClick(e) {
+    this.stop()
+    this.setState({
+      isSimRunning: false,
+      iterations: 0,
+      lastRoll: new Array(this.state.diceCount).fill("?"),
+      resultCounts: new Array(5 * this.state.diceCount + 1).fill(0),
+      avgSumHist: [],
     })
   }
 
   render() {
     let diceList = []
-    for (let dieVal of this.state.lastRoll) {
-      diceList.push(<Die value={dieVal} />)
+    for (let i in this.state.lastRoll) {
+      diceList.push(<Die key={i} value={this.state.lastRoll[i]} />)
+    }
+
+    let avgDisplay = " - "
+    if (this.state.avgSumHist.length > 0) {
+      avgDisplay =
+        Math.round(
+          this.state.avgSumHist[this.state.avgSumHist.length - 1] * 1000
+        ) / 1000
     }
 
     return (
       <Layout>
         <AppIntro introHTML={intro} />
-        <div
-          className="containter"
-          style={{ maxWidth: "300px", margin: "auto" }}
-        >
+
+        <div className="container d-flex justify-content-around text-center">
+          <DataDisplay title="Iterations" data={this.state.iterations} />
+
           <div id="settings-bin">
             <InputField
               title="Number of Dice"
@@ -110,14 +220,38 @@ class DiceApp extends React.Component {
               onChange={this.diceCountOnChange}
             />
           </div>
+
+          <DataDisplay title="Average Sum" data={avgDisplay} />
         </div>
+
         <div
           id="dice-bin"
           className="container d-flex flex-wrap justify-content-center"
         >
           {diceList}
         </div>
-        <div id="stats-bin"></div>
+        <div id="control-btns" className="container text-center">
+          <div className="btn-group">
+            <ControlBtn
+              css={ctrlBtnStyle}
+              className="btn btn-lg"
+              text="Start"
+              onClick={this.startOnClick}
+            />
+            <ControlBtn
+              css={ctrlBtnStyle}
+              className="btn btn-lg"
+              text="Stop"
+              onClick={this.stopOnClick}
+            />
+          </div>
+          <ControlBtn
+            css={resetBtnStyle}
+            className="btn btn-lg"
+            text="Reset"
+            onClick={this.resetOnClick}
+          />
+        </div>
       </Layout>
     )
   }
